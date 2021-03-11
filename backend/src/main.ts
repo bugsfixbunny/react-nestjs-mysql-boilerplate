248d4444b8e57b3161eacc6bf3e6f709f2cdaa95
goldenmonster <goldenmonster@gmail.com>
2021-03-11 10:40:19 +0100

    feat: add Logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: true,
+  });
   app.enableCors({
     origin: true,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  console.log('Listened on PORT 3000');
+  Logger.log('Listened on PORT 3000');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- backend/src/shared/guards/auth.guard.ts
+++ backend/src/shared/guards/auth.guard.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
 import { Reflector } from '@nestjs/core';
 import { ConfigService } from '../config/config.service';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -12,12 +13,10 @@ export class AuthGuard implements CanActivate {
 
     canActivate(context: ExecutionContext,): boolean | Promise<boolean> | Observable<boolean> {
         const roles = this.reflector.get<string>('roles', context.getHandler());
-        const time = new Date();
-        const timeFormat = ('0' + time.getHours()).slice(-2) + ":" + ('0' + time.getMinutes()).slice(-2) + ":" + ('0' + time.getSeconds()).slice(-2);
         const request = context.switchToHttp().getRequest();
 
         if (!roles) {
-            console.log(`${timeFormat}  ${request.route['stack'][0].method} ${request.originalUrl}`);
+            Logger.log(`${request.route['stack'][0].method} ${request.originalUrl}`);
             return true;
         }
 
@@ -28,19 +27,19 @@ export class AuthGuard implements CanActivate {
                 const user = jwt.verify(token, this.config.environment.secretKey);
                 const role = user['role'];
                 if (roles.includes(role)) {
-                    console.log(`${timeFormat}  ${request.route['stack'][0].method} ${request.originalUrl} authorized`);
+                    Logger.log(`${request.route['stack'][0].method} ${request.originalUrl} authorized`);
                     request.user = user;
                     return true;
                 }
             } catch (err) {
-                console.log(`${timeFormat}  ${request.route['stack'][0].method} ${request.originalUrl} unauthorized bad token`);
+                Logger.log(`${request.route['stack'][0].method} ${request.originalUrl} unauthorized bad token`);
                 throw new HttpException({
                     status: HttpStatus.UNAUTHORIZED,
                     error: err,
                 }, 401);
             }
         }
-        console.log(`${timeFormat}  ${request.route['stack'][0].method} ${request.originalUrl} unauthorized no token`);
+        Logger.log(`${request.route['stack'][0].method} ${request.originalUrl} unauthorized no token`);
         return false;
     }
 }
